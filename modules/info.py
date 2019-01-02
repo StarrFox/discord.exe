7b248775e16647d15ac122dfa1c1506222508ddb
StarrFox <StarrFox6312@gmail.com>
2019-01-02 06:10:47 -0600

    userinfo command

--- modules/info.py
+++ modules/info.py
@@ -10,6 +10,7 @@ class info:
 
     @commands.command()
     async def info(self, ctx):
+        """View bot info"""
         owner = "StarrFox#6312"
         guilds = len(self.bot.guilds)
         users = len(self.bot.users)
@@ -24,10 +25,30 @@ class info:
         e.add_field(name="Owner:", value=owner)
         e.add_field(name="Connected to:", value=f"Guilds: {guilds}\nChannels: {channels}\nUsers: {users}")
         e.add_field(name="Top role", value=f"{top_role} in pos #{top_role_pos}")
-        e.add_field(name="Uptime", value=f"been up since {uptime}")
+        e.add_field(name="Uptime", value=f"Been up since {uptime}")
         e.add_field(name="Support server", value=f"[Join here]({support_server})")
         e.add_field(name="Invite me", value=f"[Invite here]({invite})")
         await ctx.send(embed=e)
+
+    @commands.command(aliases=['ui'])
+    async def userinfo(self, ctx, user: discord.Member = None):
+        """Get info on a server member"""
+        if not user:
+            user = ctx.author
+        joined = humanize.naturaldate(user.joined_at)
+        joined_dis = humanize.naturaldate(user.create_at)
+        top_role = user.top_role.name
+        top_role_pos = (ctx.message.guild.roles[::-1].index(user.top_role))+1
+        e = discord.Embed()
+        e.add_field(name="Name|id", value=f"{user.name}|{user.id}")
+        e.set_thumbnail(url=user.avatar_url)
+        e.add_field(name="Joined guild:", joined)
+        e.add_field(name="Joined discord:", joined_dis)
+        e.add_field(name="Status:", value=f"Web: {user.web_stats}\nDesktop: {user.desktop_status}\nMobile: {user.mobile_status}")
+        e.add_field(name="Top role:", value=f"{top_role} in pos #{top_role_pos}")
+        e.add_field(name=f"{len(user.roles)} roles:", value=f"{[r.mention for r in user.roles]}")
+        await ctx.send(embed=e)
+
 
 def setup(bot):
     bot.add_cog(info(bot))
