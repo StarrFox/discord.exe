51f00991b532c4bd33ec31dfbb1aaad5a149d82d
StarrFox <StarrFox6312@gmail.com>
2019-01-07 05:28:20 -0600

    import typo

--- modules/tags.py
+++ modules/tags.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 from fuzzywuzzy import process
 from discord.ext.commands.cooldowns import BucketType
-from paginator import paginator
+from utils.paginator import paginator
 
 class tags:
 
