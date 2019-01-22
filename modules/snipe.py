504c52b4e556a5a862de7c878c9f506c569b9585
StarrFox <StarrFox6312@gmail.com>
2019-01-21 19:45:30 -0600

    snipe command

---
+++ modules/snipe.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import asyncio
+import typing
+
+class snipe:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.snipe_dict = {}
+
+    async def on_message_delete(self, msg):
+        """Saves message to snipe dict"""
+        if not msg.content:
+            return
+        if not msg.channel.id in self.snipe_dict:
+            self.snipe_dict[msg.channel.id] = []
+        self.snipe_dict[msg.channel.id].append(msg)
+
+    @commands.command(name='snipe')
+    async def _snipe(self, ctx, channel: typing.Optional[discord.TextChannel] = None, index: int = 0):
+        """Snipe a command deleted in a channel"""
+        if index < 0:
+            return await ctx.send("Positive numbers only")
+        if not channel:
+            channel = ctx.channel
+        if not channel.id in self.snipe_dict:
+            return await ctx.send("This channel has no recorded messages")
+        if len(self.snipe_dict)-1 < index:
+            return await ctx.send("I don't have that many messages recorded")
+        msg = self.snipe_dict[channel.id][index]
+        e = discord.Embed(
+            color=discord.Color.dark_purple(),
+            description=msg.content
+        )
+        e.set_author(
+            name=msg.author.name,
+            icon_url=msg.author.avatar_url
+        )
+        e.set_footer(
+            text=f"{index}/{len(self.snipe_dict[channel.id])}"
+        )
+        await ctx.send(embed=e)
+
+def setup(bot):
+    bot.add_cog(snipe(bot))
